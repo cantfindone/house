c2a99061718b9105fcc9a596e3528309c317fc9b
leonado.lu <leonado.lu@qq.com>
2018-03-28 16:39:42 +0800

    add wx post handler

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,24 @@
 // index:
 
+const wechat  = require('../wx/wechat'), 
 
-var sha1 = require('sha1');
+sha1 = require('sha1');
  
 var config = {
-  wechat:{
     appID:'wx97e2bad23693131d', //填写你自己的appID
     appSecret:'b62ef8b1881bf202cc5b6a0553eb5ae9', //填写你自己的appSecret
-    token:'987654321' //填写你自己的token
-  }
+    token:'987654321',//填写你自己的token
+	encodingAESKey:"afPsLj0rV0yGBtZRyWpGWTGFgsrvI40XBCZrnwUURFk",
+    apiDomain:"https://api.weixin.qq.com/",
+    apiURL:{
+        accessTokenApi:"%scgi-bin/token?grant_type=client_credential&appid=%s&secret=%s",
+        createMenu:"%scgi-bin/menu/create?access_token=%s"
+    }
 };
- 
+
+var wechatApp = new wechat(config); //实例wechat 模块
+
+console.log('instanciate wechatapp....:'+ wechatApp) ;
 
 module.exports = {
     'GET /': async (ctx, next) => {
@@ -22,7 +30,7 @@ module.exports = {
 	
 	'GET /wx': async (ctx, next) => {
 		console.log('body: '+JSON.stringify(ctx.query));
-		var token = config.wechat.token;
+		var token = config.token;
 		var signature = ctx.query.signature;
 		var nonce = ctx.query.nonce;
 		var timestamp = ctx.query.timestamp;
@@ -32,5 +40,16 @@ module.exports = {
         //ctx.response.type = 'application/json';
 		// 设置Response Body:
 		ctx.response.body = (sha === signature)? echostr + '' : 'failed'; //比较并返回结果
+    },
+	
+	'POST /wx': async (ctx, next) => {
+		console.log('body: '+JSON.stringify(ctx.request.body));
+		wechatApp.handleMsg(ctx);
+    },
+	
+	'GET /wx/tk': async (ctx, next) => {
+		console.log('body: '+JSON.stringify(ctx.query));
+		var tk= await wechatApp.getAccessToken();
+		ctx.response.body=tk;
     }
 };
