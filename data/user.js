21914b14589f6f1edc5e3bd9fdc89c80c3fb4ff2
leonado.lu <leonado.lu@qq.com>
2018-06-29 21:03:51 +0800

    init

---
+++ data/user.js
@@ -0,0 +1,5 @@
+db.user.insert({_id:'ping',password:'123',prv:'10'})
+
+
+db.prv.insert({_id:'10',asst:['doctor','doctorExt','user','herb']})
+db.prv.insert({_id:'2',asst:['doctor','doctorExt','user','herb']})
