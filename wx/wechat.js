b19cc8da2fa99095b77e556bb5a264785ae353bc
leonado.lu <leonado.lu@qq.com>
2018-07-03 21:29:02 +0800

    wx

--- wx/wechat.js
+++ wx/wechat.js
@@ -223,11 +223,9 @@ WeChat.prototype.handleMsg = function(ctx){
              //根据消息内容返回消息信息
              switch(result.Content[0]){
                  case '1':
-                     reportMsg = msg.txtMsg(fromUser,toUser,'Hello ！我的英文名字叫 H-VK');
+                     reportMsg = msg.txtMsg(fromUser,toUser,'你好~');
                  break;
-                 case '2':
-                     reportMsg = msg.txtMsg(fromUser,toUser,'Node.js是一个开放源代码、跨平台的JavaScript语言运行环境，采用Google开发的V8运行代码,使用事件驱动、非阻塞和异步输入输出模型等技术来提高性能，可优化应用程序的传输量和规模。这些技术通常用于数据密集的事实应用程序');
-                 break;
+
                  case '文章':
                      var contentArr = [
                          {Title:"Node.js 微信自定义菜单",Description:"使用Node.js实现自定义微信菜单",PicUrl:"http://img.blog.csdn.net/20170605162832842?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast",Url:"http://blog.csdn.net/hvkcoder/article/details/72868520"},
@@ -238,8 +236,13 @@ WeChat.prototype.handleMsg = function(ctx){
                      reportMsg = msg.graphicMsg(fromUser,toUser,contentArr);
                  break;
                  default:
-                     reportMsg = msg.txtMsg(fromUser,toUser,'https://api.lunary.top');
-                 break;
+                        var contentArr = [
+                            {Title:"月亮之上",Description:"月亮之上",Url:"https://api.lunary.top"}
+                           ];
+                        //回复图文消息
+                        reportMsg = msg.graphicMsg(fromUser,toUser,contentArr);
+                    break;
+                 
              }
          }
      }
